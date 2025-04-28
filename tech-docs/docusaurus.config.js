4e02009002101a2ab5543a3885a5862c5d8fd3ae
Sathish Thangavel <sathish.thangavel@solitontech.local>
2025-04-28 12:28:49 +0530

    feat: add config deployment

--- tech-docs/docusaurus.config.js
+++ tech-docs/docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sk2111.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tech-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'sk2111', // Usually your GitHub org/user name.
+  projectName: 'tech-docs', // Usually your repo name.
+  deploymentBranch: "main",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
